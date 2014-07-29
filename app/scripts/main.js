20422e9b3391192f97149628be380b0f26e38b30
Tyler Sloan <tyler.g.sloan@gmail.com>
2014-07-29 16:40:37 -0400

    refactored all js to use prototyped objects for the days of the week+schedules

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,102 +1,69 @@
 'use strict';
 
 var d           = new Date(),
-    firstOpen   = null,
-    firstClose  = null,
-    secondOpen  = null,
-    secondClose = null,
-    openTimes   = [],
-
-    days      = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    days        = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"],
+    daysLength  = days.length,
+    defaultView = '<div><h1>sorry kid care is closed.</h1></div>';
 
 
+var scheduleProto = {
+  dayOfWeek: "",
+  firstOpen: {},
+  firstClose: {},
+  secondOpen: {},
+  secondClose: {}
+};
+
+function Schedule (dayOfWeek, firstOpen, firstClose, secondOpen, secondClose) {
+  this.dayOfWeek   = dayOfWeek;
+  this.firstOpen   = firstOpen;
+  this.firstClose  = firstClose;
+  this.secondOpen  = secondOpen;
+  this.secondClose = secondClose;
+}
+
+Schedule.prototype = scheduleProto;
+
+var sunday     =  new Schedule("Sunday","1pm","1pm","3pm","9pm");
+var monday     =  new Schedule("Monday","8am","1pm","3pm","9pm");
+var tuesday    =  new Schedule("Tuesday","8am","1pm","3pm","9pm");
+var wednesday  =  new Schedule("Wednesday","8am","1pm","3pm","9pm");
+var thursday   =  new Schedule("Thursday","8am","1pm","3pm","9pm");
+var friday     =  new Schedule("Friday","9am","3pm");
+var saturday   =  new Schedule("Saturday","1pm","4pm");
 
 function everyMinute() {
 // runs every 60 sec and runs on init.
-  var hour          = d.getHours(),
-      nextHour      = hour + 1,
-      day           = d.getDay(),
-      nextDay       = day + 1,
-      currentMinute = d.getMinutes(),
+  var hour           = d.getHours(),
+      nextHour       = hour + 1,
+      day            = d.getDay(),
+      nextDay        = day + 1,
+      currentMinute  = d.getMinutes(),
+      minTilNextHour = 60 - currentMinute,
 
       body          = $('body');
 
-  console.log(hour + ':' + currentMinute, nextHour);
+  if (day === 0) { day = sunday    }
+  if (day === 1) { day = monday    }
+  if (day === 2) { day = tuesday   }
+  if (day === 3) { day = wednesday }
+  if (day === 4) { day = thursday  }
+  if (day === 5) { day = friday    }
+  if (day === 6) { day = saturday  }
 
-  var defaultView =
-      '<div>' +
-      '<h1>sorry kid care is closed.</h1>' +
-      '</div>';
+  console.log(hour + ':' + currentMinute, minTilNextHour + " minutes until " + (nextHour - 12) + " oclock.");
 
-  // Sunday
-  if( day === 0 ) {
-    firstOpen  = 1,
-    firstClose = 3,
-    openTimes  = [firstOpen + '-' + firstClose],
-    day        = "Sunday",
-    nextDay    = "Monday";
+  $('#js-day').html(day.dayOfWeek);
 
-    if( hour >= 13 && hour < 15 ) {
-      console.log('it is open');
-    } else { body.html(defaultView) }
+  $('#js-first-open').html(day.firstOpen);
+  $('#js-first-close').html(day.firstClose);
 
-    console.log(nextDay)
+  if(day.secondOpen != 'undefined' && day.secondClose != 'undefined') {
+    $('#js-second-open').html(day.secondOpen);
+    $('#js-second-close').html(day.secondClose);
   }
 
-  // monday through thursday
-  if( day === 1 || day === 2 || day === 3 || day === 4 ) {
-    firstOpen = 8,
-    firstClose = 1,
-    secondOpen = 4,
-    secondClose = 9,
-    openTimes = [firstOpen + '-' + firstClose, secondOpen + '-' + secondClose],
-    day = "M-Th";
-
-    if( hour >= 8 && hour < 13 || hour >= 16 && hour < 21 ) {
-      console.log('it is open');
-    }
-  }
-
-  // Friday
-  if( day === 5 ) {
-    firstOpen = 8,
-    firstClose = 1,
-    secondOpen = 4,
-    secondClose = 7,
-    openTimes = [firstOpen + '-' + firstClose, secondOpen + '-' + secondClose],
-    day = "Friday";
-
-    console.log('It is Friday and Kid Care opens at ' + firstOpen);
-
-    if( hour >= 8 && hour < 13 || hour >= 16 && hour < 19 ) {
-      body.css('background', 'green');
-      console.log('it is open');
-    }
-  }
-
-  // Saturday
-  if( day === 6 ) {
-    firstOpen = 9,
-    firstClose = 3,
-    openTimes = [firstOpen + '-' + firstClose],
-    day = "Saturday";
-
-    console.log('It is Saturday and Kid Care opens at ' + firstOpen);
-
-    if( hour >= 8 && hour < 13 || hour >= 16 && hour < 19 ) {
-      console.log('it is open');
-    }
-  }
-
-  $('#js-opentimes').html(openTimes);
-  $('#js-timeleft').html(60 - currentMinute)
-  $('#js-day').html(day);
-
-  if( secondOpen === null ) {
-    secondOpen = nextDay;
-  }
-
-  console.log('It is ' + day + '. Kid Care is open from ' + firstOpen + '-' + firstClose + ' and ' + secondOpen + '-' + secondClose);
+  // $('#js-timeleft').html(minTilNextHour)
 };
 
 everyMinute();
