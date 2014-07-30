313ea9e928cef2b71f79d3207aa94388b2dd7060
Tyler Sloan <tyler.g.sloan@gmail.com>
2014-07-30 00:17:29 -0400

    cleaned up js and added comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var today       = new Date(),
-    days        = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"],
-    daysLength  = days.length,
-    defaultView = '<div><h1>sorry kid care is closed.</h1></div>';
-
-
+/*
+ * The Schedule Object
+ * It's a way to add a schedule to each day of the week.
+ */
 var scheduleProto = {
   dayOfWeek: "",
   firstOpen: {},
@@ -20,11 +18,11 @@ function Schedule (dayOfWeek, firstOpen, firstClose, secondOpen, secondClose) {
   this.firstClose  = firstClose;
   this.secondOpen  = secondOpen;
   this.secondClose = secondClose;
-}
+};
 
 Schedule.prototype = scheduleProto;
 
-var sunday     =  new Schedule("Sunday","1pm","1pm","3pm","9pm");
+var sunday     =  new Schedule("Sunday","1pm","4pm");
 var monday     =  new Schedule("Monday","8am","1pm","3pm","9pm");
 var tuesday    =  new Schedule("Tuesday","8am","1pm","3pm","9pm");
 var wednesday  =  new Schedule("Wednesday","8am","1pm","3pm","9pm");
@@ -32,15 +30,23 @@ var thursday   =  new Schedule("Thursday","8am","1pm","3pm","9pm");
 var friday     =  new Schedule("Friday","9am","3pm");
 var saturday   =  new Schedule("Saturday","1pm","4pm");
 
-function everyMinute() {
-// runs every 60 sec and runs on init.
+/*
+ * app() is, you know, the whole thing.
+ * First I set vars for things that make up the time,
+ * then I turn the day returned into a word, like Monday, etc.
+ * Then I add data to the HTML.
+ */
+
+function app() {
   var today   = new Date(),
       h       = today.getHours(),
       day     = today.getDay(),
       m       = today.getMinutes(),
       s       = today.getSeconds(),
       m       = checkTime(m),
-      s       = checkTime(s);
+      s       = checkTime(s),
+
+      defaultView = '<h3>Sorry, Kid Care is closed.</h3>';
 
   if (day === 0) { day = sunday    }
   if (day === 1) { day = monday    }
@@ -60,21 +66,57 @@ function everyMinute() {
     $('#js-second-close').html(day.secondClose);
   }
 
+  /*
+   * The following checks what day it is and changes layout accordingly
+   */
+
+  if(day === monday || tuesday || wednesday || thursday || friday) {
+    if (h < 8 || h > 12 && h < 15 || h > 20) {
+      $('.hero-unit').html(defaultView)
+    } else {
+      $('.hero-unit').css({
+        "background": "green"
+      })
+    }
+  }
+
+  if(day === saturday) {
+    if (h < 9 && h > 14) {
+      $('.hero-unit').html(defaultView)
+    } else {
+      $('.hero-unit').css({
+        "background": "green"
+      })
+    }
+  }
+
+  if(day === sunday) {
+    if (h < 13 && h > 15) {
+      $('.hero-unit').html(defaultView)
+    } else {
+      $('.hero-unit').css({
+        "background": "green"
+      })
+    }
+  }
+
+  /*
+   * Hats of to, ehem, W3C Docs for this clock functionality
+   */
+
   var t = setTimeout( function () {
-    everyMinute()
+    app()
   }, 500);
 
   function checkTime(i) {
     if (i<10) {
       i = "0" + i
-    };  // add zero in front of numbers < 10
+    };
     return i;
   }
 
-  document.getElementById('clock').innerHTML = h+":"+m+":"+s;
-
-
+  $('#clock').html(h+":"+m+":"+s)
+  
 };
 
-everyMinute();
-// setInterval(everyMinute, 5*1000);
+app();
