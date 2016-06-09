fd948b12f1ad97b1992157047095b382c862f94f
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-06-08 22:47:16 -0400

    get and print the current time

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentHour:   new Date().getHours(),
+			currentMinute: new Date().getMinutes(),
+			currentSecond: new Date().getSeconds()
+		}
+	}
 	render() {
+		console.log(this.state.currentSecond)
 		return (
-		  <div>Hi</div>
+			<h1>{this.state.currentHour}:{this.state.currentMinute}:{this.state.currentSecond}</h1>
 		)
 	}
-
-	// methods
 }
 
 export default App
