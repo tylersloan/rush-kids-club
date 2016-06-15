98dbe53b417f472e6e60c0689eec5634084ee57c
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-06-15 15:01:42 -0400

    remove helpers.js

--- helpers.js
+++
@@ -1,7 +0,0 @@
-let helpers = {
-	titleCase: function(word) {
-		return word.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-	}
-}
-
-export default helpers
