2fe4bd20de2bb63f14f35ac26027490400a2e2b5
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-06-16 17:07:28 -0400

    change reduxstagram to kidsclub

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './client/reduxstagram'
+    './client/kidsclub'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
