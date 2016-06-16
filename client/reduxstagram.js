bef3de17a37c151d7662a0719bb01a40ed4620c5
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-06-16 17:05:06 -0400

    bonk css for now

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,12 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = React.createClass({
+	render() {
+		return (
+			<h1>hello</h1>
+		)
+	}
+});
+
+render(<App />, document.getElementById('root'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// var css = require("./client/styles/styles.scss");
+// returns compiled css code from styles.scss, resolves Sass imports
+// var css = require("!css!sass!./styles.scss");
+// returns compiled css code from styles.scss, resolves Sass and CSS imports and url(...)s
+
 module.exports = {
   devtool: 'source-map',
   entry: [
@@ -23,13 +28,13 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'client')
-    },
-    // CSS
-    {
-      test: /\.scss$/,
-      include: path.join(__dirname, 'client'),
-      loaders: ["style", "css", "sass"]
     }
+    // CSS
+    // {
+    //   test: /\.scss$/,
+    //   include: path.join(__dirname, 'client'),
+    //   loaders: ["style", "css", "sass"]
+    // }
     ]
   }
 };
