0efe4b3455de3a6f7039e1db09fa83fc3b988427
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-06-17 15:55:46 -0400

    comment console.logs

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -6,14 +6,14 @@ import { routerReducer, LOCATION_CHANGE } from 'react-router-redux';
 const initialState = { content : {} };
 
 export default function app_reducer(state={initialState},action){
-	console.log("app_reducer")
+	// console.log("app_reducer")
 	switch(action.type){
 		case 'CLEAR_CHUNK':
-			console.log("initialState: ", initialState)
+			// console.log("initialState: ", initialState)
 			return Object.assign({},initialState);
 		case 'GET_CHUNK':
 		    var {content} = action.payload;
-		    console.log("action: ", action)
+		    // console.log("action: ", action)
 		    return Object.assign({},state.content,{
 		    	content
 		    });
